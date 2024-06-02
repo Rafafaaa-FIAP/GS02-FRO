b15d981fdf768f13672f7e53e838c19de0f94c22
Rafafaaa <cristofalirafael@gmail.com>
2024-06-01 21:07:09 -0300

    fix: routes

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,11 +1,11 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 
 import App from '../App'
 import Error from './Error'
 import Login from './Login'
 import Home from './Home'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: <App />,
