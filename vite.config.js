8e4d58a6728cfc3c148b37f36e73bfefd1593d42
Rafafaaa <cristofalirafael@gmail.com>
2024-08-22 20:10:06 -0300

    configs

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/GS02-FRO",
+  base: "/GS02-front",
   build: { chunkSizeWarningLimit: 1600, }
 })
